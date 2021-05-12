e038314bbafde2e83b09000a1564804faa3f4d36
Quinten Cabo <tintin10q@hotmail.com>
2021-05-13 00:10:36 +0200

    Fixed

--- test/simple_test.py
+++ test/simple_test.py
@@ -4,11 +4,10 @@ import utils
 class TestAvailableModelUtilFunctions(unittest.TestCase):
     """This test suite tests the functions that give access to the config file"""
 
-    def test_conf_exist(self):
+    def test_existed_var(self):
         """This function tests if the config file for the translation server exists"""
         self.assertIsInstance(utils.get_env_var("FLASKKEY"), str, "get_env_var() could not find a flask Key")
 
-    def test_get_conf(self):
+    def test_nonexisted_var(self):
         """This function tests if the get_conf function returns a dictionary with the config file"""
-        self.assertIsInstance(utils.get_env_var("None"), str, "get_env_var")
-
+        self.assertIsInstance(
